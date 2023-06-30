39debd7b87b906391ade0bb2b0a831e49d51e00b
NorbertStefaniszyn <norbertstefaniszyn@gmail.com>
2023-07-01 00:05:30 +0200

    Dodanie SkryptHelp.sh

---
+++ SkryptHelp.sh
@@ -0,0 +1,6 @@
+#!binbash
+echo Dostępne opcje
+echo - SkryptData.sh Wyświetla dzisiejszą datę.
+echo - SkryptLogs.sh Tworzy automatycznie 100 plików logx.txt, x - numer pliku od 1-100, wpisuje nazwę pliku, nazwę skryptu i datę.
+echo - SkryptLogs30.sh Tworzy automatycznie określoną liczbę plików logx.txt, x - numer pliku, wpisuje nazwę pliku, nazwę skryptu i datę.
+echo - SkryptHelp.sh Wyświetla wszystkie dostępne opcje.
